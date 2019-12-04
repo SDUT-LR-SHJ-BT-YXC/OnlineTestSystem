043f39349ddc75fb04c5511e93effcd4468daeb4
ACacti <shangjinv6@163.com>
2019-12-04 23:19:08 +0800

    完成部分在线答题功能

--- src/cn/OnlineTestSystem/web/servlet/ExaminationServlet.java
+++ src/cn/OnlineTestSystem/web/servlet/ExaminationServlet.java
@@ -5,20 +5,22 @@ import cn.OnlineTestSystem.service.ExaminationService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/ExaminationServlet")
 public class ExaminationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
         String id = request.getParameter("id");
         ExaminationService examinationService = new ExaminationService();
         ExaminationPaper paper = examinationService.createExaminationPaper(Integer.parseInt(id), 10, 10, 10);
         HttpSession session = request.getSession();
-        //Something ......
+        session.setAttribute("paper", paper);
+        Cookie cookie = new Cookie("JSESSIONID", session.getId());
+        cookie.setMaxAge(60 * 40);
+        response.addCookie(cookie);
+        request.getRequestDispatcher(request.getContextPath() + "/client/Examination.jsp");
 
     }
 
