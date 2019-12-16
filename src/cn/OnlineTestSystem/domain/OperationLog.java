b09192b67ce9a7424c59de86b1b685bd4d4668d7
yxc9806 <903608066@qq.com>
2019-12-16 19:16:43 +0800

    新建adminpage.jsp（管理员操作记录界面） 新建usermanagement.jsp(用户管理界面) 新建OperationLogServlet.java(获取管理员操作记录) 新建usermanagementServlet.java(获取用户信息) 新建OperationLog.java（管理员操作记录类）

---
+++ src/cn/OnlineTestSystem/domain/OperationLog.java
@@ -0,0 +1,15 @@
+package cn.OnlineTestSystem.domain;
+
+/**
+ * @ClassName OperationLog
+ * @Desciption 管理员操作记录 类
+ * @Author yinxiaochen
+ * @Date 2019-12-16 18:35
+ * @Version 1.0
+ **/
+
+public class OperationLog {
+    public OperationLog() {
+    }
+
+}
